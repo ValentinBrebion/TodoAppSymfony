4efe6c67d85ae278debd9c75c9cb147b8fad9639
Valentin <vb.brebionpro@gmail.com>
2024-06-23 16:47:48 +0200

    landingpage

---
+++ src/Controller/LandingHomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class LandingHomeController extends AbstractController
+{
+    #[Route('/', name: 'app_landing_home')]
+    public function index(): Response
+    {
+        return $this->render('landing_home/index.html.twig', [
+            
+        ]);
+    }
+
+    #[Route('/todoConnexion', name: 'app_connexion')]
+    public function connexion(): Response
+    {
+        return $this->render('landing_home/todoConnexion.html.twig', [
+            
+        ]);
+    }
+
+    #[Route('/todoInscription', name: 'app_inscription')]
+    public function inscription(): Response
+    {
+        return $this->render('landing_home/todoInscription.html.twig', [
+            
+        ]);
+    }
+}
