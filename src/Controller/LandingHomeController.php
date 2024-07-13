fc59f802f66014307a627dca83ff6c348de1dbc6
Valentin <vb.brebionpro@gmail.com>
2024-07-13 12:38:30 +0200

    test phpcbf for github action

--- src/Controller/LandingHomeController.php
+++ src/Controller/LandingHomeController.php
@@ -11,28 +11,36 @@ class LandingHomeController extends AbstractController
     #[Route('/', name: 'app_landing_home')]
     public function index(): Response
     {
-        return $this->render('landing_home/index.html.twig', [     
-        ]);
+        return $this->render(
+            'landing_home/index.html.twig', [     
+            ]
+        );
     }
 
     #[Route('/todoConnexion', name: 'app_connexion')]
     public function connexion(): Response
     {
-        return $this->render('landing_home/todoConnexion.html.twig', [    
-        ]);
+        return $this->render(
+            'landing_home/todoConnexion.html.twig', [    
+            ]
+        );
     }
 
     #[Route('/todoInscription', name: 'app_inscription')]
     public function inscription(): Response
     {
-        return $this->render('landing_home/todoInscription.html.twig', [      
-        ]);
+        return $this->render(
+            'landing_home/todoInscription.html.twig', [      
+            ]
+        );
     }
 
     #[Route('/homePage', name: 'app_homepage')]
     public function homePage(): Response
     {
-        return $this->render('landing_home/homePage.html.twig', [
-        ]);
+        return $this->render(
+            'landing_home/homePage.html.twig', [
+            ]
+        );
     }
 }
